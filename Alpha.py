7967bf8841148a0977dc4fd352ebeba81336a506
Jhonattan Lino <63247251+Jhonattanln@users.noreply.github.com>
2021-05-06 18:04:53 -0300

    Update Alpha.py

--- Alpha.py
+++ Alpha.py
@@ -1,2 +1,14 @@
 import pandas as pd
 import statsmodels.formula.api as smf
+
+
+#### Importando os dados
+funds = pd.read_excel('economatica.xlsx', parse_dates=True, index_col=0)
+hml = pd.read_excel('HML_Factor.xlsx', parse_dates=True, index_col=0)
+mark = pd.read_excel('Market_Factor.xlsx', parse_dates=True, index_col=0)
+smb = pd.read_excel('SMB_Factor.xlsx', parse_dates=True, index_col=0)
+wml = pd.read_excel('WML_Factor.xlsx', parse_dates=True, index_col=0)
+
+
+
+factor = pd.concat([mark, hml, smb, wml], axis=1)
