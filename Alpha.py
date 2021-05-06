134a1da3748a9267a4d7f57515e9b5eff1e8aa44
Jhonattan Lino <63247251+Jhonattanln@users.noreply.github.com>
2021-05-06 19:03:40 -0300

    Update Alpha.py

--- Alpha.py
+++ Alpha.py
@@ -9,6 +9,15 @@ mark = pd.read_excel('Market_Factor.xlsx', parse_dates=True, index_col=0)
 smb = pd.read_excel('SMB_Factor.xlsx', parse_dates=True, index_col=0)
 wml = pd.read_excel('WML_Factor.xlsx', parse_dates=True, index_col=0)
 
+##### Ajustando nomes das colunas
+
+def columns(df):
+    df.columns = df.columns.str[43:]
+columns(funds)
+
+#### Concatenando dados
+funds = funds.pct_change()
+factor = pd.concat([funds, mark, hml, smb, wml], axis=1).dropna()
 
 
-factor = pd.concat([mark, hml, smb, wml], axis=1)
+
