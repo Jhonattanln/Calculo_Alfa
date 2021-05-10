a9f7dca3b06bead514fa8a39fd1d5d281c5c68c8
Jhonattan Lino <63247251+Jhonattanln@users.noreply.github.com>
2021-05-10 11:36:02 -0300

    Update Alpha.py

--- Alpha.py
+++ Alpha.py
@@ -8,6 +8,7 @@ hml = pd.read_excel('HML_Factor.xlsx', parse_dates=True, index_col=0)
 mark = pd.read_excel('Market_Factor.xlsx', parse_dates=True, index_col=0)
 smb = pd.read_excel('SMB_Factor.xlsx', parse_dates=True, index_col=0)
 wml = pd.read_excel('WML_Factor.xlsx', parse_dates=True, index_col=0)
+#Fama = pd.read_excel('FamaFrenchFactor.xlsx', parse_dates=True, index_col=0)
 
 ##### Ajustando nomes das colunas
 
@@ -17,7 +18,32 @@ columns(funds)
 
 #### Concatenando dados
 funds = funds.pct_change()
-factor = pd.concat([funds, mark, hml, smb, wml], axis=1).dropna()
+factor = pd.concat([mark, hml, smb, wml, funds['4um Small Caps FIA']], axis=1).dropna()
+print(factor.describe())
 
+#### Regressão
+"""
+rsquared = pd.DataFrame()
+fama_df = pd.DataFrame()
+
+for i in funds.columns:
+    rsquared[i] = smf.ols(formula=f'{i} ~ Market_Factor + HML + SMB + WML', data=factor)
+    rsquared[i] = rsquared[i].fit()
+    fama_df[i] = rsquared[i].rsquared_adj
+
+print(fama_df)
+"""
+
+"""
+# Define the regression formula
+FamaFrench_model = smf.ols(formula = '4um Small Caps FIA ~ SMB', data=factor)
+
+# Fit the regression
+FamaFrench_fit = FamaFrench_model.fit()
+
+# Extract the adjusted r-squared
+regression_adj_rsq = FamaFrench_fit.rsquared_adj
+print(regression_adj_rsq)
+"""
 
 
