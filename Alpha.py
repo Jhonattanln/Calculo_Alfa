24e3d952621c83d83fa4f25e70de6693d910c61c
Jhonattan Lino <63247251+Jhonattanln@users.noreply.github.com>
2021-06-10 23:45:02 -0300

    Update Alpha.py

--- Alpha.py
+++ Alpha.py
@@ -22,25 +22,31 @@ columns(funds)
 #### Concatenando dados
 funds = funds.pct_change()
 factor = pd.concat([mark, hml, smb, wml, funds], axis=1).dropna()
-#print(factor.head())
+print(factor.columns)
 
 #### Regressão
-funds_name = funds.columns
+
 
 def reg(y):
     X = add_constant(factor[['Market_Factor', 'HML', 'SMB', 'WML']])
     Y = y
     fit = smf.OLS(Y, X).fit()
-    p_value = fit.pvalues['const']
-    r_2 = fit.rsquared_adj
-    alpha = fit.params[0]
-    
+    var = {'p_values': fit.pvalues['Market_Factor'],
+            'r_2': fit.rsquared_adj,
+            'alpha': fit.params[0]}
+    return var
+
+x = reg(factor['Xp Investor Dividendos 30 Fc FIA'])
+print(x)
+
+"""
 alfa = pd.DataFrame()
 
 for i in funds.columns:
-    alfa[i] = reg(factor[i])
+    x = reg(factor[i])
+    alfa['p_value'] = x['p_values']
 
 print(alfa)
-
+"""
 
 
